8c0125e423733f10d339c96094c88855b95d5f6a
Bernado Santos <bernardosmm2004@gmail.com>
2023-10-24 16:08:48 -0300

    stash inicio view

--- DominoMath/View Model/InicioView.swift
+++ DominoMath/View Model/InicioView.swift
@@ -1,21 +1,63 @@
-//
-//  InicioView.swift
-//  DominoMath
-//
-//  Created by User on 24/10/23.
-//
-
 import SwiftUI
 
 struct InicioView: View {
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        NavigationView {
+            ZStack {
+                Color(hex: "2B8B81")
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack(spacing: 0) {
+                    
+                    Image("oi")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 400, height: 230)
+                        
+                        
+                    
+                    NavigationLink(destination: ContentView()) {
+                        Text("Jogar")
+                            .foregroundColor(Color(hex: "FDAA22"))
+                            .padding(EdgeInsets(top: 20, leading: 50, bottom: 20, trailing: 50))
+                            .background(Color(hex: "E74F36"))
+                            .cornerRadius(20)
+                    
+                    }
+                }
+            }
+           
+        }
     }
 }
 
+struct OutraTelaView: View {
+    var body: some View {
+        Text("Esta é a outra tela")
+            .navigationBarTitle("Outra Tela")
+            .navigationBarBackButtonHidden(true) // Oculta o botão de retorno
+    }
+}
 
-struct InicioView_Preview: PreviewProvider {
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgb: UInt64 = 0
+
+        scanner.scanHexInt64(&rgb)
+
+        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue)
+    }
+}
+
+struct InicioView_Previews: PreviewProvider {
     static var previews: some View {
         InicioView()
     }
 }
+
+
