64670216ed8a99f4059651c761d138172d51d658
Bernado Santos <bernardosmm2004@gmail.com>
2023-10-24 15:01:56 -0300

    Criação de novo Swift File de nome InicioView

---
+++ DominoMath/View Model/InicioView.swift
@@ -0,0 +1,21 @@
+//
+//  InicioView.swift
+//  DominoMath
+//
+//  Created by User on 24/10/23.
+//
+
+import SwiftUI
+
+struct InicioView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+
+struct InicioView_Preview: PreviewProvider {
+    static var previews: some View {
+        InicioView()
+    }
+}
