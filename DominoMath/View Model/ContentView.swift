bc8282a2e554ee82e6cd35582cf0e3a5caf5aaf4
Bernado Santos <bernardosmm2004@gmail.com>
2023-11-10 16:42:05 -0300

    Alteracao ordem ordem stacks content view

--- DominoMath/View Model/ContentView.swift
+++ DominoMath/View Model/ContentView.swift
@@ -8,43 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    
     var body: some View {
         //ScrollView([.horizontal]) {
-            VStack {
-                ZStack(alignment: .topLeading) {
-                    DominoBoard()
-                    HStack {
-                        ForEach(Array(dominoes.prefix(6))) { dominoset in
-                            Domino(domino: dominoset)
-                        }
+        VStack {
+            ZStack(alignment: .topLeading) {
+                DominoBoard()
+                HStack {
+                    ForEach(Array(dominoes.prefix(6))) { dominoset in
+                        Domino(domino: dominoset)
+                            .position(x: 100, y: 50) // Ajuste a posição conforme necessário
                     }
-                    .padding(.top, 100)
                 }
-                ZStack {
-                    
-                }
-                ZStack {
-                    HStack {
-                        ForEach(Array(dominoes.prefix(6))) { dominoset in
-                            Domino(domino: dominoset)
-                        }
-                    }
-                    .frame(maxWidth: .infinity, alignment: .bottomLeading)
-                    .offset(y: -200)
+                .padding(.top, 100)
+            }
+
+            // Certifique-se de que a HStack esteja após a DominoBoard no ZStack
+            HStack {
+                ForEach(Array(dominoes.prefix(6))) { dominoset in
+                    Domino(domino: dominoset)
+                        .position(x: 100, y: 50) // Ajuste a posição conforme necessário
                 }
             }
+            .frame(maxWidth: .infinity, alignment: .bottomLeading)
+            .offset(y: -200)
+
+            // Outras ZStacks ou conteúdo, se necessário
+        }
         //}
         .navigationBarBackButtonHidden(true)
     }
-
-    
 }
-    
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
+}
