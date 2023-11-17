04505e0248d6fa68987a7911af9b6aa3285e0965
taissa-rodrigues <141452801+taissa-rodrigues@users.noreply.github.com>
2023-11-17 08:37:15 -0300

    Mudanças no OnBoarding

--- DominoMath/View Model/OnboardingView.swift
+++ DominoMath/View Model/OnboardingView.swift
@@ -4,49 +4,52 @@
 //
 //  Created by User on 16/11/23.
 //
-
 import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
-        
         ZStack {
             Color(.white).ignoresSafeArea()
-            VStack(alignment: .leading, spacing: 10) {
-                // Conteúdo do onboarding (regras)
-                Text("Bem-vindo ao DominoMath!")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding()
-                    .foregroundColor(.blue)
+
+            VStack {
+                Spacer()
+
                 Text("Regras")
                     .font(.largeTitle)
                     .fontWeight(.bold)
+                    .foregroundColor(.black)
                     .padding()
-                    .foregroundColor(.blue)
-                
+                    .frame(maxWidth: .infinity, alignment: .center)
 
-                Text("Cada jogador começa com 6 peças aleatórias.")
-                    .font(.headline)
-                    .foregroundColor(.black)
+                Spacer()
 
-                Text("Cada peça tem duas expressões matemáticas, uma em cada ponta.")
-                    .font(.headline)
-                    .foregroundColor(.black)
+                ScrollView {
+                    VStack(alignment: .center, spacing: 15) {
+                        Text("Cada jogador começa com 6 peças aleatórias.")
+                            .font(.title)
+                            .foregroundColor(.black)
 
-                Text("O objetivo é resolver as expressões o mais rápido possível para encaixá-las no tempo determinado.")
-                    .font(.headline)
-                    .foregroundColor(.black)
+                        Text("Cada peça tem duas expressões matemáticas, uma em cada ponta.")
+                            .font(.title)
+                            .foregroundColor(.black)
+
+                        Text("O objetivo é resolver as expressões o mais rápido possível para encaixá-las no tempo determinado.")
+                            .font(.title)
+                            .foregroundColor(.black)
+                    }
+                    .padding(20) // Adicionar espaço ao redor dos textos
+                }
+
+                Spacer()
             }
-//            .padding(20) // Adicionar espaço ao redor de todo o conteúdo
-           /* .background(Color.yellow)*/ // Cor de fundo
-            .ignoresSafeArea()
-            .cornerRadius(20)
-            .padding(.horizontal, 20)
-            .transition(.offset(y: UIScreen.main.bounds.height))
         }
-        
-        
-      
     }
 }
+
+// Preview do layout no Canvas
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
+
